74c84a049f0506c262a5b876a71cf6965d8a1ce1
Evgenija-P <e.a.poduzova@gmail.com>
2022-10-11 12:45:08 +0300

    result

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import { Statistics } from 'components/Statistics/Statistics';
 import { Notification } from 'components/Notification/Notification';
@@ -8,55 +8,60 @@ import { Section } from 'components/Section/Section';
 import { AppWrapper } from './App.styled';
 import { ButtonWrapper } from '../FeedbackOptions/FeedbackOptions.styled';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const counter = ({ option }) => {
+    switch (option) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+
+      default:
+        return 0;
+    }
   };
 
-  counter = key => {
-    this.setState(prevState => {
-      return { [key]: prevState[key] + 1 };
-    });
-  };
-
-  countTotalFeedback() {
-    return Object.values(this.state).reduce((acc, value) => acc + value, 0);
+  function countTotalFeedback() {
+    let total = good + neutral + bad;
+    return total;
   }
 
-  countPositiveFeedbackPercentage() {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  function countPositiveFeedbackPercentage() {
+    return Math.round((good / countTotalFeedback()) * 100);
   }
 
-  render() {
-    const btnTitle = Object.keys(this.state);
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-
-    return (
-      <AppWrapper>
-        <Section title="Please leave feedback">
-          <ButtonWrapper>
-            <FeedbackOptions
-              options={btnTitle}
-              onLeaveFeedback={this.counter}
-            />
-          </ButtonWrapper>
-          {total !== 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </AppWrapper>
-    );
-  }
-}
+  return (
+    <AppWrapper>
+      <Section title="Please leave feedback">
+        <ButtonWrapper>
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={counter}
+          />
+        </ButtonWrapper>
+        {countTotalFeedback() !== 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </AppWrapper>
+  );
+};
--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -6,29 +6,33 @@ import { Button } from './FeedbackOptions.styled';
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <div>
-      {options.map(btnTitle => (
+      {options.map(option => (
         <Button
           type="button"
-          key={btnTitle}
-          btnTitle={btnTitle}
-          style={{ backgroundColor: getRandomColor(btnTitle) }}
-          onClick={() => onLeaveFeedback(btnTitle)}
+          key={option}
+          name={option}
+          style={{ backgroundColor: getRandomColor(option) }}
+          onClick={() => onLeaveFeedback({ option })}
         >
-          {btnTitle}
+          {option}
         </Button>
       ))}
     </div>
   );
 };
 
-function getRandomColor(btnTitle) {
-  if (btnTitle === 'good') {
-    return 'green';
-  } else if (btnTitle === 'neutral') {
-    return 'orange';
+const getRandomColor = option => {
+  switch (option) {
+    case 'good':
+      return 'green';
+
+    case 'neutral':
+      return 'orange';
+
+    default:
+      return 'red';
   }
-  return 'red';
-}
+};
 
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
